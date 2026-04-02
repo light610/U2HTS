a5f1659c82ddbc2aebbc3c9555b8bb4f8ebf312f
CNflysky <cnflysky@qq.com>
2026-04-02 18:56:36 +0800

    bump core & controller commit supress compiler warnings

--- src/u2hts_rp2.c
+++ src/u2hts_rp2.c
@@ -30,7 +30,7 @@ inline void u2hts_i2c_init(uint32_t speed_hz) {
 }
 
 // not implemented
-inline void u2hts_spi_init(u2hts_spi_config *config) {}
+inline void u2hts_spi_init(u2hts_spi_config* config) {}
 
 inline bool u2hts_spi_transfer(void* buf, size_t len) { return false; }
 
@@ -51,7 +51,7 @@ inline void u2hts_i2c_set_speed(uint32_t speed_hz) {
 inline void u2hts_delay_ms(uint32_t ms) { sleep_ms(ms); }
 inline void u2hts_delay_us(uint32_t us) { sleep_us(us); }
 
-inline bool u2hts_usb_init() { return tud_init(BOARD_TUD_RHPORT); }
+inline void u2hts_usb_init() { tud_init(BOARD_TUD_RHPORT); }
 
 inline uint16_t u2hts_get_timestamp() {
   return (uint16_t)(to_us_since_boot(time_us_64()) / 100);
@@ -328,7 +328,8 @@ inline void u2hts_ts_irq_init(U2HTS_IRQ_TYPES irq_type) {
                                      u2hts_rp2_irq_cb);
 }
 
-inline void u2hts_usb_report(uint8_t report_id, const u2hts_hid_report* report) {
+inline void u2hts_usb_report(uint8_t report_id,
+                             const u2hts_hid_report* report) {
   tud_hid_report(report_id, report, sizeof(u2hts_hid_report));
   u2hts_usb_status = false;
 }
