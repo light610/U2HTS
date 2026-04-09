21e6d75577d6b6e550c10368da619f08431c31f2
light610 <57819098+light610@users.noreply.github.com>
2026-04-09 16:49:40 +0800

    Update u2hts_main.c

--- u2hts_main.c
+++ u2hts_main.c
@@ -1,136 +1,285 @@
-/*
-  Copyright (C) CNflysky.
-  U2HTS stands for "USB to HID TouchScreen".
-  This file is licensed under GPL V3.
-  All rights reserved.
-*/
+/**
+ * Raspberry Pi Pico USB HID Touchscreen Simulator - Draws a circle continuously
+ * 
+ * Based on U2HTS project structure. Generates a single contact moving in a
+ * circular path around screen center and sends HID reports.
+ */
 
-#include "pico/binary_info.h"
-#include "u2hts_core.h"
-#include "u2hts_rp2.h"
+#include <math.h>
+#include <string.h>
 
-#define U2HTS_BI_INFO_CONTROLLER_TAG 0x0000
-#define U2HTS_BI_INFO_CONTROLLER_ID 0x0000
+#include "bsp/board_api.h"
+#include "pico/stdlib.h"
+#include "tusb.h"
 
-#define U2HTS_BI_INFO_COORDINATE_CONFIG 0x0001
-#define U2HTS_BI_INFO_COORDINATE_ID 0x0000
+// -------------------- HID Report Definitions --------------------
+#define U2HTS_HID_TP_REPORT_ID       1
+#define U2HTS_HID_TP_MAX_COUNT_ID    2
 
-#define U2HTS_BI_INFO_BUS_CONFIG 0x0002
-#define U2HTS_BI_INFO_BUS_ID 0x0000
+// Logical and physical maximum coordinates (defines screen resolution)
+#define U2HTS_LOGICAL_MAX            2048
+#define U2HTS_PHYSICAL_MAX_X         2048
+#define U2HTS_PHYSICAL_MAX_Y         2048
 
-#define U2HTS_BI_INFO_MISC_CONFIG 0x0003
-#define U2HTS_BI_INFO_MISC_ID 0x0000
+// Number of simulated touch points (we use only one)
+#define MAX_TOUCH_POINTS             5
 
-int main() {
-  stdio_init_all();
-  u2hts_pins_init();
-  // Export config to picotool.
-  // Change with `picotool config -s <cfg> <val> U2HTS.uf2`
-  // E.g. `picotool config -s x_max 1920 U2HTS.uf2`
-  bi_decl(bi_program_feature_group(U2HTS_BI_INFO_CONTROLLER_TAG,
-                                   U2HTS_BI_INFO_CONTROLLER_ID,
-                                   "Controller Selection"));
-  // controller name
-  bi_decl(bi_ptr_string(U2HTS_BI_INFO_CONTROLLER_TAG,
-                        U2HTS_BI_INFO_CONTROLLER_ID, controller, "auto", 32));
-  bi_decl(bi_program_feature_group(U2HTS_BI_INFO_COORDINATE_CONFIG,
-                                   U2HTS_BI_INFO_COORDINATE_ID,
-                                   "Coordinate Configuration"));
-  // invert X axis
-  bi_decl(bi_ptr_int32(U2HTS_BI_INFO_COORDINATE_CONFIG,
-                       U2HTS_BI_INFO_COORDINATE_ID, x_invert, false));
-  // invert Y axis
-  bi_decl(bi_ptr_int32(U2HTS_BI_INFO_COORDINATE_CONFIG,
-                       U2HTS_BI_INFO_COORDINATE_ID, y_invert, false));
-  // swap X and Y
-  bi_decl(bi_ptr_int32(U2HTS_BI_INFO_COORDINATE_CONFIG,
-                       U2HTS_BI_INFO_COORDINATE_ID, x_y_swap, false));
-  // max touch points
-  bi_decl(bi_ptr_int32(U2HTS_BI_INFO_COORDINATE_CONFIG,
-                       U2HTS_BI_INFO_COORDINATE_ID, max_tps, 0));
-  // max X coordinate
-  bi_decl(bi_ptr_int32(U2HTS_BI_INFO_COORDINATE_CONFIG,
-                       U2HTS_BI_INFO_COORDINATE_ID, x_max, 0));
-  // max Y coordinate
-  bi_decl(bi_ptr_int32(U2HTS_BI_INFO_COORDINATE_CONFIG,
-                       U2HTS_BI_INFO_COORDINATE_ID, y_max, 0));
-  bi_decl(bi_program_feature_group(U2HTS_BI_INFO_BUS_CONFIG,
-                                   U2HTS_BI_INFO_BUS_ID, "Bus Configuration"));
-  // bus type
-  bi_decl(bi_ptr_int32(U2HTS_BI_INFO_BUS_CONFIG, U2HTS_BI_INFO_BUS_ID, bus_type,
-                       UB_I2C));
-  // override I2C config
-  bi_decl(bi_ptr_int32(U2HTS_BI_INFO_BUS_CONFIG, U2HTS_BI_INFO_BUS_ID,
-                       override_i2c_config, 0));
-  // controller I2C address
-  bi_decl(bi_ptr_int32(U2HTS_BI_INFO_BUS_CONFIG, U2HTS_BI_INFO_BUS_ID, i2c_addr,
-                       0x00));
-  // override I2C speed
-  bi_decl(bi_ptr_int32(U2HTS_BI_INFO_BUS_CONFIG, U2HTS_BI_INFO_BUS_ID,
-                       i2c_speed, 0x00));
-  // override SPI config
-  bi_decl(bi_ptr_int32(U2HTS_BI_INFO_BUS_CONFIG, U2HTS_BI_INFO_BUS_ID,
-                       override_spi_config, 0));
-  // override SPI speed
-  bi_decl(bi_ptr_int32(U2HTS_BI_INFO_BUS_CONFIG, U2HTS_BI_INFO_BUS_ID,
-                       spi_speed, 0x00));
-  // override SPI cpol
-  bi_decl(bi_ptr_int32(U2HTS_BI_INFO_BUS_CONFIG, U2HTS_BI_INFO_BUS_ID, spi_cpol,
-                       0));
-  // override SPI cpha
-  bi_decl(bi_ptr_int32(U2HTS_BI_INFO_BUS_CONFIG, U2HTS_BI_INFO_BUS_ID, spi_cpha,
-                       0));
-  bi_decl(bi_program_feature_group(
-      U2HTS_BI_INFO_MISC_CONFIG, U2HTS_BI_INFO_MISC_ID, "Misc Configuration"));
-  // IRQ flag
-  bi_decl(bi_ptr_int32(U2HTS_BI_INFO_MISC_CONFIG, U2HTS_BI_INFO_MISC_ID,
-                       irq_type, 0));
-  // Report delay
-  bi_decl(bi_ptr_int32(U2HTS_BI_INFO_MISC_CONFIG, U2HTS_BI_INFO_MISC_ID,
-                       report_delay, 0));
-  // Polling mode
-  bi_decl(bi_ptr_int32(U2HTS_BI_INFO_MISC_CONFIG, U2HTS_BI_INFO_MISC_ID,
-                       polling_mode, 0));
+// Macro to build HID descriptor for one touch point
+#define U2HTS_HID_TP_DESC \
+  HID_USAGE(0x22), HID_COLLECTION(HID_COLLECTION_LOGICAL), \
+  HID_USAGE(0x42), HID_LOGICAL_MAX(1), HID_REPORT_SIZE(1), HID_REPORT_COUNT(1), \
+    HID_INPUT(HID_DATA | HID_VARIABLE | HID_ABSOLUTE), \
+  HID_REPORT_COUNT(7), HID_INPUT(HID_CONSTANT | HID_VARIABLE | HID_ABSOLUTE), \
+  HID_REPORT_SIZE(8), HID_USAGE(0x51), HID_REPORT_COUNT(1), \
+    HID_INPUT(HID_DATA | HID_VARIABLE | HID_ABSOLUTE), \
+  HID_USAGE_PAGE(HID_USAGE_PAGE_DESKTOP), \
+  HID_LOGICAL_MAX_N(U2HTS_LOGICAL_MAX, 2), HID_REPORT_SIZE(16), \
+  HID_USAGE(HID_USAGE_DESKTOP_X), \
+  HID_PHYSICAL_MAX_N(U2HTS_PHYSICAL_MAX_X, 2), \
+    HID_INPUT(HID_DATA | HID_VARIABLE | HID_ABSOLUTE), \
+  HID_PHYSICAL_MAX_N(U2HTS_PHYSICAL_MAX_Y, 2), \
+  HID_USAGE(HID_USAGE_DESKTOP_Y), \
+    HID_INPUT(HID_DATA | HID_VARIABLE | HID_ABSOLUTE), \
+  HID_USAGE_PAGE(HID_USAGE_PAGE_DIGITIZER), \
+  HID_LOGICAL_MAX_N(255, 2), HID_PHYSICAL_MAX_N(255, 2), \
+  HID_REPORT_SIZE(8), HID_REPORT_COUNT(3), \
+  HID_USAGE(0x48), HID_USAGE(0x49), HID_USAGE(0x30), \
+    HID_INPUT(HID_DATA | HID_VARIABLE | HID_ABSOLUTE), \
+  HID_COLLECTION_END
 
-  bi_decl(bi_ptr_string(U2HTS_BI_INFO_MISC_CONFIG, U2HTS_BI_INFO_MISC_ID,
-                        custom_controller_config, "",
-                        U2HTS_CUSTOM_CONFIG_STR_MAX_TOTAL_LENGTH));
+// Descriptor for scan time and contact count
+#define U2HTS_HID_TP_INFO_DESC \
+  HID_LOGICAL_MAX_N(0xFFFF, 3), HID_REPORT_SIZE(16), HID_UNIT_EXPONENT(0x0C), \
+  HID_UNIT_N(0x1001, 2), HID_REPORT_COUNT(1), HID_USAGE(0x56), \
+    HID_INPUT(HID_DATA | HID_VARIABLE | HID_ABSOLUTE), \
+  HID_USAGE(0x54), HID_LOGICAL_MAX(MAX_TOUCH_POINTS), HID_REPORT_SIZE(8), \
+    HID_INPUT(HID_DATA | HID_VARIABLE | HID_ABSOLUTE)
 
-  u2hts_config cfg = {.controller = controller,
-                      .bus_type = bus_type,
-                      .override_i2c_config = override_i2c_config,
-                      .i2c_config =
-                          {
-                              .addr = i2c_addr,
-                              .speed_hz = i2c_speed,
-                          },
-                      .override_spi_config = override_spi_config,
-                      .spi_config =
-                          {
-                              .cpol = spi_cpol,
-                              .cpha = spi_cpha,
-                              .speed_hz = spi_speed,
-                          },
-                      .coord_config =
-                          {
-                              .x_invert = x_invert,
-                              .y_invert = y_invert,
-                              .x_y_swap = x_y_swap,
-                              .max_tps = max_tps,
-                              .x_max = x_max,
-                              .y_max = y_max,
-                          },
-                      .irq_type = irq_type,
-                      .polling_mode = polling_mode,
-                      .custom_controller_config = custom_controller_config};
-  U2HTS_ERROR_CODES ret = u2hts_init(&cfg);
-  if (ret)
-    while (1)
-#ifdef U2HTS_ENABLE_LED
-      u2hts_led_show_error_code(ret);
-#endif
-  while (1) {
-    tud_task();
-    u2hts_task();
-  }
+// Feature report for maximum contacts
+#define U2HTS_HID_TP_MAX_COUNT_DESC \
+  HID_USAGE(0x55), HID_FEATURE(HID_DATA | HID_VARIABLE | HID_ABSOLUTE)
+
+// -------------------- Data Structures --------------------
+typedef struct __packed {
+  bool    contact;   // Tip switch (1 = touch present)
+  uint8_t id;        // Contact identifier
+  uint16_t x;        // X coordinate
+  uint16_t y;        // Y coordinate
+  uint8_t width;     // Contact width
+  uint8_t height;    // Contact height
+  uint8_t pressure;  // Contact pressure
+} u2hts_tp;
+
+typedef struct __packed {
+  u2hts_tp tp[MAX_TOUCH_POINTS];
+  uint16_t scan_time;   // Scan time in 0.1ms units
+  uint8_t  tp_count;    // Number of valid contacts in this report
+} u2hts_hid_report;
+
+// -------------------- USB Descriptors --------------------
+static const tusb_desc_device_t device_desc = {
+    .bLength            = sizeof(device_desc),
+    .bDescriptorType    = TUSB_DESC_DEVICE,
+    .bcdUSB             = 0x0200,
+    .bDeviceClass       = 0x00,
+    .bDeviceSubClass    = 0x00,
+    .bDeviceProtocol    = 0x00,
+    .bMaxPacketSize0    = CFG_TUD_ENDPOINT0_SIZE,
+    .idVendor           = 0x2E8A,   // Raspberry Pi
+    .idProduct          = 0x8572,   // 'UH' in ASCII
+    .bcdDevice          = 0x0100,
+    .iManufacturer      = 0x01,
+    .iProduct           = 0x02,
+    .iSerialNumber      = 0x03,
+    .bNumConfigurations = 0x01
+};
+
+static const uint8_t hid_report_desc[] = {
+    HID_USAGE_PAGE(HID_USAGE_PAGE_DIGITIZER),
+    HID_USAGE(0x04),                         // Touch screen
+    HID_COLLECTION(HID_COLLECTION_APPLICATION),
+
+    HID_REPORT_ID(U2HTS_HID_TP_REPORT_ID),
+    HID_USAGE(0x22),                         // Finger
+    HID_PHYSICAL_MIN(0),
+    HID_LOGICAL_MIN(0),
+    HID_UNIT_EXPONENT(0x0E),
+    HID_UNIT(0x11),
+
+    // Repeat for each possible contact (5 points)
+    U2HTS_HID_TP_DESC,
+    U2HTS_HID_TP_DESC,
+    U2HTS_HID_TP_DESC,
+    U2HTS_HID_TP_DESC,
+    U2HTS_HID_TP_DESC,
+
+    U2HTS_HID_TP_INFO_DESC,
+
+    HID_REPORT_ID(U2HTS_HID_TP_MAX_COUNT_ID),
+    U2HTS_HID_TP_MAX_COUNT_DESC,
+
+    HID_COLLECTION_END
+};
+
+static const uint8_t config_desc[] = {
+    TUD_CONFIG_DESCRIPTOR(1, 1, 0, TUD_CONFIG_DESC_LEN + TUD_HID_DESC_LEN,
+                          TUSB_DESC_CONFIG_ATT_REMOTE_WAKEUP, 100),
+    TUD_HID_DESCRIPTOR(0, 0, HID_ITF_PROTOCOL_NONE, sizeof(hid_report_desc),
+                       0x81, CFG_TUD_HID_EP_BUFSIZE, 5)
+};
+
+static uint16_t _desc_str[32 + 1];
+static const uint8_t *string_desc_arr[] = {
+    (const uint8_t[]){0x09, 0x04},          // Language: English (US)
+    (const uint8_t *)"U2HTS",
+    (const uint8_t *)"USB HID Touch Simulator",
+    NULL                                     // Serial number filled later
+};
+
+// -------------------- TinyUSB Callbacks --------------------
+uint8_t const *tud_descriptor_device_cb(void) {
+    return (uint8_t const *)&device_desc;
+}
+
+uint8_t const *tud_hid_descriptor_report_cb(uint8_t instance) {
+    (void)instance;
+    return hid_report_desc;
+}
+
+uint8_t const *tud_descriptor_configuration_cb(uint8_t index) {
+    (void)index;
+    return config_desc;
+}
+
+uint16_t const *tud_descriptor_string_cb(uint8_t index, uint16_t langid) {
+    (void)langid;
+    size_t chr_count;
+
+    switch (index) {
+        case 0:
+            memcpy(&_desc_str[1], string_desc_arr[0], 2);
+            chr_count = 1;
+            break;
+        case 3:
+            chr_count = board_usb_get_serial(_desc_str + 1, 32);
+            break;
+        default:
+            if (index >= sizeof(string_desc_arr) / sizeof(string_desc_arr[0]))
+                return NULL;
+            const uint8_t *str = string_desc_arr[index];
+            chr_count = strlen((const char *)str);
+            if (chr_count > 31) chr_count = 31;
+            for (size_t i = 0; i < chr_count; i++)
+                _desc_str[1 + i] = str[i];
+            break;
+    }
+
+    _desc_str[0] = (uint16_t)((TUSB_DESC_STRING << 8) | (2 * chr_count + 2));
+    return _desc_str;
+}
+
+// -------------------- HID Report Handlers --------------------
+static bool host_ready = false;   // Set after host requests max contacts
+
+void tud_hid_set_report_cb(uint8_t instance, uint8_t report_id,
+                           hid_report_type_t report_type,
+                           uint8_t const *buffer, uint16_t bufsize) {
+    (void)instance; (void)report_id; (void)report_type;
+    (void)buffer; (void)bufsize;
+    // Not used
+}
+
+uint16_t tud_hid_get_report_cb(uint8_t instance, uint8_t report_id,
+                               hid_report_type_t report_type,
+                               uint8_t *buffer, uint16_t reqlen) {
+    (void)instance;
+    if (report_type == HID_REPORT_TYPE_FEATURE &&
+        report_id == U2HTS_HID_TP_MAX_COUNT_ID) {
+        buffer[0] = MAX_TOUCH_POINTS;
+        host_ready = true;   // Host is ready to receive touch reports
+        return 1;
+    }
+    return 0;
+}
+
+void tud_hid_report_complete_cb(uint8_t instance, uint8_t const *report,
+                                uint16_t len) {
+    (void)instance; (void)report; (void)len;
+    // Report sent, ready for next
+}
+
+// -------------------- Circle Drawing Logic --------------------
+#define CIRCLE_CENTER_X   (U2HTS_PHYSICAL_MAX_X / 2)
+#define CIRCLE_CENTER_Y   (U2HTS_PHYSICAL_MAX_Y / 2)
+#define CIRCLE_RADIUS     500
+#define STEP_ANGLE        0.1f      // radians per step
+
+static float angle = 0.0f;
+
+// Fill a HID report with a single touch at current angle
+static void prepare_circle_report(u2hts_hid_report *report) {
+    memset(report, 0, sizeof(*report));
+
+    // Compute coordinates on circle
+    uint16_t x = (uint16_t)(CIRCLE_CENTER_X + CIRCLE_RADIUS * cosf(angle));
+    uint16_t y = (uint16_t)(CIRCLE_CENTER_Y + CIRCLE_RADIUS * sinf(angle));
+
+    // Clamp to valid range
+    if (x > U2HTS_PHYSICAL_MAX_X) x = U2HTS_PHYSICAL_MAX_X;
+    if (y > U2HTS_PHYSICAL_MAX_Y) y = U2HTS_PHYSICAL_MAX_Y;
+
+    // Fill first contact
+    report->tp[0].contact  = true;
+    report->tp[0].id       = 1;
+    report->tp[0].x        = x;
+    report->tp[0].y        = y;
+    report->tp[0].width    = 30;
+    report->tp[0].height   = 30;
+    report->tp[0].pressure = 30;
+
+    report->tp_count = 1;
+    report->scan_time = (uint16_t)(to_us_since_boot(time_us_64()) / 100); // 0.1ms units
+}
+
+// -------------------- Main --------------------
+int main(void) {
+    stdio_init_all();
+    board_init();
+    tud_init(BOARD_TUD_RHPORT);
+
+    // Wait for USB to be connected and configured
+    while (!tud_mounted()) {
+        tud_task();
+        sleep_ms(10);
+    }
+
+    // Wait for host to request max contacts (sets host_ready)
+    while (!host_ready) {
+        tud_task();
+        sleep_ms(10);
+    }
+
+    // Main loop: send touch reports to draw a circle
+    while (1) {
+        tud_task();
+
+        u2hts_hid_report report;
+        prepare_circle_report(&report);
+
+        // Send report if USB is ready and not busy
+        if (tud_hid_ready()) {
+            tud_hid_report(U2HTS_HID_TP_REPORT_ID, &report, sizeof(report));
+        }
+
+        // Advance angle for next point
+        angle += STEP_ANGLE;
+        if (angle > 2.0f * M_PI) {
+            angle -= 2.0f * M_PI;
+        }
+
+        // Delay to control drawing speed (~10ms per point gives smooth motion)
+        sleep_ms(10);
+    }
+
+    return 0;
 }
